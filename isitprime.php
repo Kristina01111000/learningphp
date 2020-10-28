c15b4bd3854cdda65ccdfbf544afe68dc48438da
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-10-28 15:08:24 -0400

    if statement and setting variables

--- isitprime.php
+++ isitprime.php
@@ -1,3 +1,14 @@
+<?php 
+  if (is_numeric($_GET["number"])&&$_GET["number"]>0&&$_GET["number"]==round($_GET["number"],0))
+    $i=2;
+    $isPrime=true;
+
+
+?>
+
+
+
+
 <p>Type in a number to see if it is prime!</p>
 <form>
   <input name="number" type="text">
