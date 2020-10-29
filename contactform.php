0836371f261e05bd7243a5f9feaf45c361f27c2a
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-10-29 15:32:42 -0400

    added html head

--- contactform.php
+++ contactform.php
@@ -1,1 +1,24 @@
-
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content = "text/html; charset = UTF-8">
+		<meta charset= "utf-8">
+		<meta name="viewport" content= "width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="assets/img/favicon.ico">
+		<!-- font from google-->
+		<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500;700&display=swap" rel="stylesheet"> 
+		<title>PHP contact form</title>
+		<!-- bootstrap core css-->
+		<link href="assets/css/bootstrap.min.css" rel ="stylesheet">
+		<!-- custom css-->
+		<link href="assets/css/custom.css" rel ="stylesheet">
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+ </html>
