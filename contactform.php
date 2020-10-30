37c0c4dfc182650031da6298aca8434bc1f89f8b
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-10-30 16:29:17 -0400

    added javascript
    returns the actual error messages

--- contactform.php
+++ contactform.php
@@ -40,7 +40,28 @@
 	    </form>
 	  </div>
 	  
-    <!-- Optional JavaScript -->
+  <script type="text/javascript">
+	  $('form').submit(function(e){
+	  //  e.preventDefault();
+	    var error = '';
+	    if ($('#email').val()==''){
+	      error+="The email field is required<br>";
+	    }
+	    if ($('#subject').val()==''){
+	      error+="The subject field is required<br>";
+	    }
+	    if ($('#content').val()==''){
+	      error+="The content field is required<br>";
+	    }
+	    if (error != "") {
+		$("#error").html('<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form:</strong></p>' + error + '</div>');
+		return false;
+	      } else {
+	      //  $("form").unbind("submit").submit();
+		return true;
+	    }
+	  });
+  </script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>  
