90d1dfec1115dd8ce665d3aa861ac93ecc437a23
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-11-05 13:01:37 -0500

    comments, keywords

--- syntaxcheatsheet.php
+++ syntaxcheatsheet.php
@@ -1,6 +1,14 @@
 <?php
   // <?php        ?> side bars
-  
+  /* multiline comment
+  // single line comment
+  # single line comment
+  */  
+
+  # key words not case sensitive
+  ECHO "Hello World";
+  echo "Hello World";
+  EcHo "Hello World";
 
 
 ?>
